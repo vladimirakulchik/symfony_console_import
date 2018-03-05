082683d0f1d41f412dd3c7f0b5c260de26c7e429
Vladimir Akulchik <vladimirakulchyk@gmail.com>
2018-03-05 09:27:06 +0300

    Add ProductTest

---
+++ src/AppBundle/Tests/Entity/ProductTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Tests\Entity;
+
+use AppBundle\Entity\Product;
+
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+    public function test__construct()
+    {
+        $product = new Product();
+        $this->assertInstanceOf(Product::class, $product);
+
+        return $product;
+    }
+
+    /**
+     * @depends test__construct
+     * @param Product $product
+     */
+    public function testToArray(Product $product)
+    {
+        $array = $product->toArray();
+        $this->assertInternalType('array', $array);
+    }
+}
